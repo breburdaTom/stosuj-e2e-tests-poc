6157087f8f29297144329171a00c7ba74386096e
tomas.breburda <tomas.breburda@alteryx.com>
2024-03-26 19:56:25 +0100

    DCA kalkulacka testcases

--- cypress/e2e/dca_kalkulacka.spec.cy.js
+++ cypress/e2e/dca_kalkulacka.spec.cy.js
@@ -1,5 +1,4 @@
 /// <reference types="Cypress" />
-
 describe('DCA Kalkulacka', () => {
 
 it('Navigates to DCA Kalkulacka and performs actions on interactive elements', () => {
@@ -10,7 +9,10 @@ it('Navigates to DCA Kalkulacka and performs actions on interactive elements', (
     cy.contains('Kolik investovat? DCA kalkulačka')
 
     cy.get('[aria-haspopup="listbox"]').contains('4 roky').click()
-    // TODO: xxx
+    cy.get('li[data-value="8"]').click()
+    cy.get('[aria-haspopup="listbox"]').contains('1 týden').click()
+    cy.get('li[data-value="30"]').click()
+    cy.get('input[value="2000"]').clear().type('1000')
 })
 
 }) 
